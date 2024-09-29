2624d3f1cf7ca6ace328156ec9acbed707ff7541
motolies <knw1234@gmail.com>
2024-09-29 09:51:46 +0900

    init repository

---
+++ src/test/java/kr/hvy/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hvy.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
