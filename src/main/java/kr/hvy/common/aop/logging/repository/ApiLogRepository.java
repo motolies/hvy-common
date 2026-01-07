42bc568e475b800cfaf1fe7be0773762b48ae626
Namwon Kim <knw1234@gmail.com>
2026-01-07 18:31:07 +0900

    로그 삭제방법 수정

--- src/main/java/kr/hvy/common/aop/logging/repository/ApiLogRepository.java
+++ src/main/java/kr/hvy/common/aop/logging/repository/ApiLogRepository.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import kr.hvy.common.aop.logging.entity.ApiLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApiLogRepository extends JpaRepository<ApiLog, Long> {
 
   @Modifying
-  int deleteByCreatedAtBefore(LocalDateTime cutoffDate);
+  @Query("DELETE FROM ApiLog a WHERE a.created.at < :cutoffDate")
+  int deleteByCreatedAtBefore(@Param("cutoffDate") LocalDateTime cutoffDate);
 }
--- src/main/java/kr/hvy/common/aop/logging/repository/SystemLogRepository.java
+++ src/main/java/kr/hvy/common/aop/logging/repository/SystemLogRepository.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import kr.hvy.common.aop.logging.entity.SystemLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SystemLogRepository extends JpaRepository<SystemLog, Long> {
 
   @Modifying
-  int deleteByCreatedAtBefore(LocalDateTime cutoffDate);
+  @Query("DELETE FROM SystemLog s WHERE s.created.at < :cutoffDate")
+  int deleteByCreatedAtBefore(@Param("cutoffDate") LocalDateTime cutoffDate);
 }
