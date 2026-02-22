f69cf11e14302a7f8ecf5b2cc7270b445fa24293
Namwon Kim <knw1234@gmail.com>
2026-02-22 22:52:50 +0900

    postgres 대응 페이징

--- src/main/java/kr/hvy/common/infrastructure/database/mybatis/interceptor/PageInterceptor.java
+++ src/main/java/kr/hvy/common/infrastructure/database/mybatis/interceptor/PageInterceptor.java
@@ -63,6 +63,7 @@ public class PageInterceptor implements Interceptor {
       MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
 
       String countSql = originalSql.replaceFirst("(?is)SELECT\\s+.+?\\sFROM\\s", "SELECT COUNT(*) FROM ");
+      countSql = countSql.replaceFirst("(?is)\\sORDER\\s+BY\\s+[\\s\\S]*$", "");
       BoundSql countBoundSql = copyFromBoundSql(mappedStatement, boundSql, countSql);
 
       // invocation의 인자에서 Connection 가져오기 (prepare 메서드의 첫번째 인자)
